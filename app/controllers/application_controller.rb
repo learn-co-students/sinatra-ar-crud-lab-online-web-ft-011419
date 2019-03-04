18346ea11f73a778b1268bf66d878a53e695bc71
gbs4ever <gbs4ever2@gmail.com>
2019-03-04 13:52:54 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,23 +7,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-    get'/' do 
-    erb :index
-    end
+    
+  #get'/' do 
+   # erb :index
+    #end
     get '/articles/new' do
     erb :new
     end
 
     post '/articles' do 
      # binding.pry
-      Article.create(params)
-     redirect  '/articles/:id'
+       @article =Article.create(params)
+     redirect to   "/articles/#{ @article.id }"
     end
     
     get'/articles'  do
     @articles = Article.all
      erb :index 
     end
+    
     get'/articles/:id'  do
      @article = Article.find(params[:id])
      erb :show 
